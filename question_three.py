aba4608f96e00550c6bfee0f51c738cafbfa3ad4
letty23244 <denladunamis@gmail.com>
2024-10-05 11:29:40 +0300

    CHANGES

--- question_three.py
+++ question_three.py
@@ -8,40 +8,19 @@
 # 50% - 59%    Grade is E
 # < 50%        Fail
 # SOLUTION
-# subject_mark = int(input("Enter the mark scored: "))
-# if subject_mark >= 90:
-#     grade = "A"
-# elif subject_mark >= 80:
-#     grade = "B"
-# elif subject_mark>=70:
-#     grade = "C"
-# elif subject_mark >=60:
-#     grade = "D"
-# elif subject_mark >= 50:
-#     grade = "E"
-# else:
-#     grade = "Fail"
-# print("Grade:", grade)
-
-# OR
-def calculate_grades():
-
-    print("\n...Student Grade Calculations...")
-
-    mark = int(input("Enter the subject mark: \t"))
-    if mark >= 90 and mark<= 100:
-        print('Grade is A')
-    elif mark >= 80 and mark <= 89:
-        print ("Grade is B")
-    elif mark >= 70 and mark <= 79:
-        print('Grade is C')
-    elif mark >= 60 and mark <= 69:
-        print('Grade is D')
-    elif mark >= 50 and mark <= 59:
-        print('Grade is E')
-    else:
-        print('Fail')
-calculate_grades()     #Calling the function
+subject_mark = int(input("Enter the mark scored: "))
+if subject_mark >= 90:
+     print("Grade A")
+elif subject_mark >= 80:
+     print("Grade B")
+elif subject_mark>=70:
+     print("Grade C")
+elif subject_mark >=60:
+     print("Grade D")
+elif subject_mark >= 50:
+     print("Grade E")
+else:
+    print("Fail")
 
 
 
